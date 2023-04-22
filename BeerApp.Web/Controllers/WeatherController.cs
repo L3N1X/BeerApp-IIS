311668ae78844a9bbaa99fbeb73b77eb6b46135e
L3N1X <leon.kruslin@algebra.hr>
2023-04-22 15:21:58 +0200

    xml-rpc in progress

---
+++ BeerApp.Web/Controllers/WeatherController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BeerApp.Web.Controllers
+{
+    public class WeatherController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index()
+        {
+            XmlRpcClient client = new XmlRpcClient();
+            client.Url = "http://example.com/xmlrpc"; // Replace with the URL of the XML-RPC endpoint
+            object myResult = client.Invoke("myMethod");
+            Console.WriteLine(myResult.ToString());
+            return View();
+        }
+    }
+}
---
+++ BeerApp.Web/Models/WeatherViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BeerApp.Web.Models
+{
+    public class WeatherViewModel
+    {
+        public string? Message { get; set; }
+        public string? Temperature { get; set; }
+        public string? City { get; set; }
+    }
+}
